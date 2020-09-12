374681d6606e52c95a87558b194e449cbe0fba94
sche0176 <sche0176@student.monash.edu>
2020-09-12 20:22:06 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+let app = express();
+const actors = require('./routers/actor');
+const movies = require('./routers/movie');
+
+app.listen(8080);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://localhost:27017/MovieLibrary', function(req, res) {
+    if (err) {
+        console.log('Error in Mongoose connection: ', err);
+        throw err;
+    }
+    console.log('Successfully connected');
+});
+
+//Configuring Endpoints
+//Actor RESTFul endpoionts 
+app.get('/actors', actors.getAll);
+app.post('/addactor', actors.createOne);
+app.get('/actor/:id', actors.getOne);
+app.put('/updateactor/:id', actors.updateOne);
+app.post('/actors/:id/addmovie', actors.addMovie);
+app.delete('/deleteactor/:id', actors.deleteOne);
+app.delete('/deleteactorandmovies/:id', actors.deleteActorAndMovies);
+app.delete('/removemovie/:actorid/:movieid', actors.removeMovie);
+
+
+//Movie RESTFul  endpoints
+app.get('/movies', movies.getAll);
+app.post('/addmovie', movies.createOne);
+app.get('/movie/:id', movies.getOne);
+app.put('/updatemovie/:id', movies.updateOne);
+app.post('/movies/:id/addactor', movies.addActor);
+app.delete('/deletemovie/:id', movies.deleteOne);
+app.delete('/removeactor/:movieid/:actorid', movies.removeActor);
+app.get('/movies/:year1/:year2', movies.getMoviesRangeYears);
+app.get('/deletemovies/:year1/:year2', movies.deleteMoviesRangeYears);
---
+++ routers/movie.js
@@ -0,0 +1,92 @@
+var Actor = require('../models/actor');
+var Movie = require('../models/movie');
+const mongoose = require('mongoose');
+
+module.exports = {
+    getAll: function (req, res) {
+        Movie.find(function (err, movies) {
+            if (err) return res.status(400).json(err);
+            res.json(movies);
+        });
+    },
+    createOne: function (req, res) {
+        let newMovieDetails = req.body;
+        newMovieDetails._id = new mongoose.Types.ObjectId();
+        Movie.create(newMovieDetails, function (err, movie) {
+            if (err) return res.status(400).json(err);
+            res.json(movie);
+        });
+    },
+    getOne: function (req, res) {
+        Movie.findOne({ _id: req.params.id })
+            .populate('actors')
+            .exec(function (err, movie) {
+                if (err) return res.status(400).json(err);
+                if (!movie) return res.status(404).json();
+                res.json(movie);
+            }
+        );
+    },
+    updateOne: function (req, res) {
+        Movie.findOneAndUpdate({ _id: req.params.id }, req.body, function (err, movie) {
+            if (err) return res.status(400).json(err);
+            if (!movie) return res.status(404).json();
+            res.json(movie);
+        });
+    },
+    deleteOne: function (req, res) {
+        Movie.findOneAndRemove({ _id: req.params.id }, function (err) {
+            if (err) return res.status(400).json(err);
+            res.json();
+        });
+    },
+    removeActor: function (req, res) {
+        Movie.findOne({ _id: req.params.movieid }, function (err, movie) {
+            if (err) return res.status(400).json(err);
+            if (!movie) return res.status(404).json();
+            Actor.findOne({ _id: req.params.actorid }, function (err, actor) {
+                if (err) return res.status(400).json(err);
+                if (!actor) return res.status(404).json();
+                movie.actors.pull(actor._id);
+                movie.save(function (err) {
+                    if (err) return res.status(500).json(err);
+                    res.json(movie);
+                });
+            })
+        });
+    },
+    addActor: function (req, res) {
+        Movie.findOne({ _id: req.params.id }, function (err, movie) {
+            if (err) return res.status(400).json(err);
+            if (!movie) return res.status(404).json();
+            Actor.findOne({ _id: req.body.id }, function (err, actor) {
+                if (err) return res.status(400).json(err);
+                if (!actor) return res.status(404).json();
+                movie.actors.push(actor._id);
+                movie.save(function (err) {
+                    if (err) return res.status(500).json(err);
+                    res.json(movie);
+                });
+            })
+        });
+    },
+    getMoviesRangeYears: function (req, res) {
+        Movie.find( {year: { $lte: req.params.year1, $gte: req.params.year2} }).populate('actors').exec(
+            function (err, movies) {
+                if (err) return res.status(400).json(err);
+                if (!movies) return res.status(404).json();
+                res.json(movies);
+            }
+        );
+    },
+    deleteMoviesRangeYears: function (req, res) {
+        let rangeOfYears = req.body;
+        Movie.find( {year: { $lte: rangeOfYears.year1, $gte: rangeOfYears.year2} }, function (err, movies) 
+        {
+            if (err) return res.status(400).json(err);
+            if (!movies) return res.status(404).json();
+            Movie.deleteMany( { $in: movies } );
+            res.json();
+        });
+    },
+};
